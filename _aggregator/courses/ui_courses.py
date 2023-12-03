a3b3128be3c6c7da91f44105bf3b381b9be52690
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-12-03 03:03:23 +0100

    Added more data that contains the aggregate for each course in each university, created a test logic for printing faculties and the departments inder them in a file named merit.py

---
+++ _aggregator/courses/ui_courses.py
@@ -0,0 +1,342 @@
+ui_courses_ = {
+        "AGRICULTURAL ECONOMICS": {
+        "id": 1,
+        "faculty": "AGRICULTURE",
+        "aggregate": 52.62,
+    },
+    "AGRICULTURAL EXTENSION AND RURAL DEVELOPMENT": {
+        "id": 2,
+        "faculty": "AGRICULTURE",
+        "aggregate": 51.375,
+    },
+    "AGRONOMY": {
+        "id": 3,
+        "faculty": "AGRICULTURE",
+        "aggregate": 51.875,
+    },
+    "ANIMAL SCIENCE": {
+        "id": 4,
+        "faculty": "AGRICULTURE",
+        "aggregate": 50.75,
+    },
+    "AGRICULTURE (CPEB)": {
+        "id": 5,
+        "faculty": "AGRICULTURE",
+        "aggregate": 50,
+    },
+    "ANTHROPOLOGY": {
+        "id": 6,
+        "faculty": "ARTS",
+        "aggregate": 53,
+    },
+    "ARABIC LANGUAGE AND LITERATURE": {
+        "id": 7,
+        "faculty": "ARTS",
+        "aggregate": 53,
+    },
+    "ARCHEOLOGY": {
+        "id": 9,
+        "faculty": "ARTS",
+        "aggregate": 53,
+    },
+    "CLASSICAL STUDIES": {
+        "id": 10,
+        "faculty": "ARTS",
+        "aggregate": 52,
+    },
+    "COMMUNICATION AND LANGUAGE ARTS": {
+        "id": 11,
+        "faculty": "ARTS",
+        "aggregate": 60.75,
+    },
+    "ENGLISH LANGUAGE AND LITERATURE": {
+        "id": 12,
+        "faculty": "ARTS",
+        "aggregate": 58.62,
+    },
+    "EUROPEAN STUDIES - FRENCH": {
+        "id": 13,
+        "faculty": "ARTS",
+        "aggregate": 50.75,
+    },
+    "EUROPEAN STUDIES - GERMAN": {
+        "id": 14,
+        "faculty": "ARTS",
+        "aggregate": 55.375,
+    },
+    "EUROPEAN STUDIES - RUSSIAN": {
+        "id": 15,
+        "faculty": "ARTS",
+        "aggregate": 53.375,
+    },
+    "HISTORY": {
+        "id": 16,
+        "faculty": "ARTS",
+        "aggregate": 51,
+    },
+    "IGBO": {
+        "id": 17,
+        "faculty": "ARTS",
+        "aggregate": 50,
+    },
+    "ISLAMIC STUDIES": {
+        "id": 18,
+        "faculty": "ARTS",
+        "aggregate": 51,
+    },
+    "LINGUISTICS": {
+        "id": 19,
+        "faculty": "ARTS",
+        "aggregate": 59,
+    },
+    "MUSIC": {
+        "id": 20,
+        "faculty": "ARTS",
+        "aggregate": 50,
+    },
+    "PHILOSOPHY": {
+        "id": 21,
+        "faculty": "ARTS",
+        "aggregate": 53,
+    },
+    "RELIGIOUS STUDIES": {
+        "id": 22,
+        "faculty": "ARTS",
+        "aggregate": 54,
+    },
+    "THEATRE ARTS": {
+        "id": 23,
+        "faculty": "ARTS",
+        "aggregate": 55.12,
+    },
+    "YORUBA": {
+        "id": 24,
+        "faculty": "ARTS",
+        "aggregate": 53,
+    },
+    "BIOCHEMISTRY": {
+        "id": 25,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 60.375,
+    },
+    "DENTISTRY": {
+        "id": 26,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 71.875,
+    },
+    "ENVIRONMENTAL HEALTH SCIENCE": {
+        "id": 27,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 55.625,
+    },
+    "HUMAN NUTRITION AND DIETETICS": {
+        "id": 28,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 57.75,
+    },
+    "MEDICAL LABORATORY SCIENCE": {
+        "id": 29,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 68.25,
+    },
+    "MEDICINE AND SURGERY": {
+        "id": 30,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 76.25,
+    },
+    "NURSING SCIENCE": {
+        "id": 31,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 68.375,
+    },
+    "PHYSIOLOGY": {
+        "id": 32,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 57.5,
+    },
+    "PHYSIOTHERAPY": {
+        "id": 33,
+        "faculty": "COLLEGE OF MEDICINE",
+        "aggregate": 66.5,
+    },
+    "LAW": {
+        "id": 34,
+        "faculty": "LAW",
+        "aggregate": 65.37,
+    },
+    "PHARMACY": {
+        "id": 35,
+        "faculty": "PHARMACY",
+        "aggregate": 69.87,
+    },
+     "AQUACULTURE AND FISHERIES MANAGEMENT": {
+        "id": 36,
+        "faculty": "RENEWABLE NATURAL RESOURCES",
+        "aggregate": 53,
+    },
+    "FOREST PRODUCTION/PRODUCTS": {
+        "id": 37,
+        "faculty": "RENEWABLE NATURAL RESOURCES",
+        "aggregate": 50,
+    },
+    "SOCIAL AND ENVIRONMENTAL FORESTRY": {
+        "id": 38,
+        "faculty": "RENEWABLE NATURAL RESOURCES",
+        "aggregate": 50,
+    },
+    "WILDLIFE AND ECOTOURISM MANAGEMENT": {
+        "id": 39,
+        "faculty": "RENEWABLE NATURAL RESOURCES",
+        "aggregate": 52,
+    },
+    "ANTHROPOLOGY": {
+        "id": 40,
+        "faculty": "SCIENCE",
+        "aggregate": 55,
+    },
+    "ARCHEOLOGY": {
+        "id": 41,
+        "faculty": "SCIENCE",
+        "aggregate": 55,
+    },
+    "BOTANY": {
+        "id": 42,
+        "faculty": "SCIENCE",
+        "aggregate": 50.625,
+    },
+    "CHEMISTRY": {
+        "id": 43,
+        "faculty": "SCIENCE",
+        "aggregate": 52,
+    },
+    "COMPUTER SCIENCE": {
+        "id": 44,
+        "faculty": "SCIENCE",
+        "aggregate": 65,
+    },
+    "GEOGRAPHY": {
+        "id": 45,
+        "faculty": "SCIENCE",
+        "aggregate": 52,
+    },
+    "GEOLOGY": {
+        "id": 46,
+        "faculty": "SCIENCE",
+        "aggregate": 50.75,
+    },
+    "INDUSTRIAL CHEMISTRY": {
+        "id": 47,
+        "faculty": "SCIENCE",
+        "aggregate": 56,
+    },
+    "MATHEMATICS": {
+        "id": 48,
+        "faculty": "SCIENCE",
+        "aggregate": 52,
+    },
+    "MICROBIOLOGY": {
+        "id": 49,
+        "faculty": "SCIENCE",
+        "aggregate": 56,
+    },
+    "PHYSICS": {
+        "id": 50,
+        "faculty": "SCIENCE",
+        "aggregate": 51,
+    },
+    "STATISTICS": {
+        "id": 51,
+        "faculty": "SCIENCE",
+        "aggregate": 52.5,
+    },
+    "ZOOLOGY": {
+        "id": 52,
+        "faculty": "SCIENCE",
+        "aggregate": 51.75,
+    },
+    "ARCHITECTURE": {
+        "id": 53,
+        "faculty": "ENVIRONMENTAL DESIGN AND MANAGEMENT",
+        "aggregate": 61,
+    },
+    "ESTATE MANAGEMENT": {
+        "id": 54,
+        "faculty": "ENVIRONMENTAL DESIGN AND MANAGEMENT",
+        "aggregate": 51,
+    },
+    "URBAN AND REGIONAL PLANNING": {
+        "id": 55,
+        "faculty": "ENVIRONMENTAL DESIGN AND MANAGEMENT",
+        "aggregate": 51,
+    },
+        "ECONOMICS": {
+        "id": 56,
+        "faculty": "ECONOMICS",
+        "aggregate": 60,
+    },
+    "GEOGRAPHY": {
+        "id": 57,
+        "faculty": "THE SOCIAL SCIENCES",
+        "aggregate": 52,
+    },
+    "POLITICAL SCIENCE": {
+        "id": 58,
+        "faculty": "THE SOCIAL SCIENCES",
+        "aggregate": 58.25,
+    },
+    "PSYCHOLOGY": {
+        "id": 59,
+        "faculty": "THE SOCIAL SCIENCES",
+        "aggregate": 56.12,
+    },
+    "SOCIOLOGY": {
+        "id": 60,
+        "faculty": "THE SOCIAL SCIENCES",
+        "aggregate": 56.5,
+    },
+    "AGRICULTURAL AND ENVIRONMENTAL ENGINEERING": {
+        "id": 61,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 50.875,
+    },
+    "CIVIL ENGINEERING": {
+        "id": 62,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 63,
+    },
+    "ELECTRICAL AND ELECTRONICS ENGINEERING": {
+        "id": 63,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 65.875,
+    },
+    "FOOD TECHNOLOGY": {
+        "id": 64,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 50.5,
+    },
+    "INDUSTRIAL AND PRODUCTION ENGINEERING": {
+        "id": 65,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 54.87,
+    },
+    "MECHANICAL ENGINEERING": {
+        "id": 66,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 68.25,
+    },
+    "PETROLEUM ENGINEERING": {
+        "id": 67,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 61,
+    },
+    "WOOD PRODUCTS ENGINEERING": {
+        "id": 68,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 53,
+    },
+    "VETERINARY MEDICINE": {
+        "id": 69,
+        "faculty": "TECHNOLOGY",
+        "aggregate": 56,
+    },
+}
--- _aggregator/universities.py
+++ _aggregator/universities.py
@@ -1,140 +1,88 @@
 # Creates a Python dictionary that stores information about universities
 from about_universities import about_uni
+from courses.ui_courses import ui_courses_
+from courses.unilag_courses import unilag_courses_
+from courses.unn_courses import unn_courses_
+from courses.oau_courses import oau_courses_
+from courses.abu_courses import abu_courses_
+from courses.unilorin_courses import unilorin_courses_
+from courses.futa_courses import futa_courses_
+from courses.unizik_courses import unizik_courses_
+from courses.uniben_courses import uniben_courses_
+from courses.fuoye_courses import fuoye_courses_
+
+
 
 universities = [
     {
         "id": 1,
         "name": "University of Ibadan (UI)",
         "about": about_uni[0]['description'],
-        "courses": [
-        {
-            "BIOCHEMISTRY": {
-            "id": 1,
-            "faculty": "COLLEGE OF MEDICINE",
-            "aggregate": 60.375,
-        },
-            "DENTISTRY": {
-                "id": 2,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 71.875,
-            },
-            "ENVIRONMENTAL HEALTH SCIENCE": {
-                "id": 3,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 55.625,
-            },
-            "HUMAN NUTRITION AND DIETETICS": {
-                "id": 4,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 57.75,
-            },
-            "MEDICAL LABORATORY SCIENCE": {
-                "id": 5,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 68.25,
-            },
-            "MEDICINE AND SURGERY": {
-                "id": 6,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 76.25,
-            },
-            "NURSING SCIENCE": {
-                "id": 7,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 68.375,
-            },
-            "PHYSIOLOGY": {
-                "id": 8,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 57.5,
-            },
-            "PHYSIOTHERAPY": {
-                "id": 9,
-                "faculty": "COLLEGE OF MEDICINE",
-                "aggregate": 66.5,
-            },
-            "LAW": {
-                    "aggregate": 73.7,
-                    "faculty": "law",
-                }
-            }
-        ]
+        "aggr_year": "2021/2022",
+        "courses": ui_courses_,
         },
         {
             "id": 2,
             "name": "University of Lagos (UNILAG)",
-            "about": about_uni[1]['description']
+            "about": about_uni[1]['description'],
+            "aggr_year": "2022/2023",
+            "courses": unilag_courses_,
         },
         {
             "id": 3,
             "name": "University of Nigeria, Nsukka (UNN)",
-            "about": about_uni[2]['description']
+            "about": about_uni[2]['description'],
+            "aggr_year": "2022/2023",
+            "courses": unn_courses_,
+
         },
         {
             "id": 4,
             "name": "Obafemi Awolowo University (OAU)",
-            "about": about_uni[3]['description']
+            "about": about_uni[3]['description'],
+            "aggr_year": "2022/2023",
+            "courses": oau_courses_,
         },
         {
             "id": 5,
             "name": "Ahmadu Bello University (ABU)",
-            "about": about_uni[4]['description']
+            "about": about_uni[4]['description'],
+            "aggr_year": None,
+            "courses": abu_courses_,
         },
         {
             "id": 6,
             "name": "University of Ilorin (UNILORIN)",
-            "about": about_uni[5]['description']
+            "about": about_uni[5]['description'],
+            "aggr_year": None,
+            "courses": unilorin_courses_,
         },
         {
             "id": 7,
             "name": "Federal University of Technology, Akure (FUTA)",
-            "about": about_uni[6]['description']
+            "about": about_uni[6]['description'],
+            "aggr_year": "2022/2023",
+            "courses": futa_courses_,
         },
         {
             "id": 8,
             "name": "Nnamdi Azikiwe University, Awka (UNIZIK)",
-            "about": about_uni[7]['description']
+            "about": about_uni[7]['description'],
+            "aggr_year": "2022/2023",
+            "courses": unizik_courses_,
         },
         {
             "id": 9,
             "name": "University of Benin (UNIBEN)",
-            "about": about_uni[8]['description']
+            "about": about_uni[8]['description'],
+            "aggr_year": "2022/2023",
+            "courses": uniben_courses_,
         },
         {
             "id": 10,
             "name": "Federal University Oye Ekiti (FUOYE)",
-            "about": about_uni[9]['description']
+            "about": about_uni[9]['description'],
+            "aggr_year": None,
+            "courses": fuoye_courses_
         }
 ]
-
-for uni in universities:
-    print("{}. {}".format(uni["id"], uni["name"]))
-print()
-
-university_id = int(input("Enter your university ID: "))
-course_of_ch = input("Enter your course of choice: ").upper()
-course_aggr = universities[0].get("courses")[0][f"{course_of_ch}"]["aggregate"]
-c_faculty = universities[0].get("courses")[0][f"{course_of_ch}"]["faculty"]
-utme_score = int(input("Enter your utme score: "))
-post_utme = int(input("Enter your post utme score: "))
-
-stu_aggr = (utme_score / 8) + ((post_utme/100) * 50)
-
-if stu_aggr >= course_aggr:
-    print(f"congratulations you met the requirements to study {course_of_ch} at {universities[university_id - 1].get('name')}")
-else: 
-    print(f"Did not meet the requirements to study {course_of_ch} at {universities[university_id - 1].get('name')}")
-print("The required score was {} and your final aggregate score was {}".format(course_aggr, stu_aggr))
-
-if c_faculty != "law":
-    print("You're qualified to study: ")
-
-
-i = 1
-for course, course_details in universities[0]["courses"][0].items():
-    if c_faculty == course_details["faculty"]:
-        if stu_aggr >= course_details["aggregate"]:
-            if course != course_of_ch:
-                print("{}. {} ({})".format(i, course, course_details["aggregate"]))
-                i += 1
-
